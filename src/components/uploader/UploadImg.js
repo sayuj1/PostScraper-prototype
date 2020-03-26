0f256861a1d0a3174fc9b61624a308efe390ce12
Sayuj <sayujsehgal@iprakash.com>
2020-03-26 08:34:13 +0530

    notification messages added!
    showing notification on uploading file.

--- src/components/uploader/UploadImg.js
+++ src/components/uploader/UploadImg.js
@@ -1,36 +1,55 @@
 import React, { Fragment, useState } from "react";
 import "antd/dist/antd";
-import { Upload, message, Modal, Button } from "antd";
+import { Upload, message, notification, Modal, Button } from "antd";
 import { UploadOutlined } from "@ant-design/icons";
+import { duration } from "moment";
 
 const UploadImg = () => {
   const [previewImg, setpreviewImg] = useState(null);
   const [previewImgShow, setpreviewImgShow] = useState(false);
   const [previewImgShowModal, setpreviewImgShowModal] = useState(false);
 
+  const openNotificationWithIcon = type => {
+    notification[type]({
+      message: "Failed Image Preview",
+      description: "Can't Preview this image!",
+      duration: 2
+    });
+  };
+
   const getBase64 = file => {
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
       reader.readAsDataURL(file);
       reader.onload = () => resolve(reader.result);
-      reader.onerror = error => reject(error);
+      reader.onerror = error => reject("error", error);
     });
   };
 
-  const handleChange = async files => {
-    // console.log(files.file);
+  const handleChange = async info => {
+    // console.log(info.file);
     // console.log("triggering");
-    const file = files.file;
+    const file = info.file;
     if (file.status === "removed") {
       setpreviewImg(null);
       setpreviewImgShow(false);
     } else {
       if (!file.url && !file.preview) {
-        file.preview = await getBase64(file.originFileObj);
+        await getBase64(file.originFileObj)
+          .then(response => (file.preview = response))
+          .catch(err => openNotificationWithIcon("error"));
       }
-
-      setpreviewImg(file.url || file.preview);
-      setpreviewImgShow(true);
+      if (file.status === "uploading") {
+        console.log(file);
+      }
+      if (file.status === "done") {
+        message.success("Image upload successfully.");
+        setpreviewImg(file.url || file.preview);
+        setpreviewImgShow(true);
+      }
+      if (file.status === "error") {
+        message.error("upload failed.");
+      }
     }
   };
 
@@ -43,9 +62,24 @@ const UploadImg = () => {
   const handleRemove = file => {
     return new Promise((resolve, reject) => {
       resolve(console.log("removing from server"));
+      message.success("Image removed successfully!");
       reject("File not removed");
     });
   };
+
+  const handleBeforeUpload = file => {
+    // console.log(file.size);
+    const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+    if (!isJpgOrPng) {
+      message.error("You can only upload JPG/PNG file!");
+    }
+    const isLt25MB = file.size / 1024 / 1024 < 25;
+    if (!isLt25MB) {
+      message.error("Image must smaller than 25MB!");
+    }
+    return isJpgOrPng && isLt25MB;
+  };
+
   return (
     <Fragment>
       <div
@@ -77,6 +111,7 @@ const UploadImg = () => {
 
       <Upload
         multiple={false}
+        beforeUpload={handleBeforeUpload}
         action="https://www.mocky.io/v2/5cc8019d300000980a055e76" // server location for uploading (upload route)
         listType="picture"
         onPreview={handlePreview}
@@ -109,7 +144,11 @@ const UploadImg = () => {
         }
       >
         <p>
-          <img alt="example" style={{ width: "100%" }} src={previewImg} />
+          <img
+            alt="Not a valid image"
+            style={{ width: "100%" }}
+            src={previewImg}
+          />
         </p>
       </Modal>
     </Fragment>
