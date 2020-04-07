52bc283d9a66cbcbd314382dff1ac5b1eb7aadb2
Sayuj Sehgal <sayujsehgal@iprakash.com>
2020-04-07 11:34:58 +0530

    user-posts-search-filter (#18)
    * user-post-search-filter added
    
    user can search its post by post-title, post-author,post-tags,post-category, and total post counter also added
    
    * fixed the invalid regexp value error
    
    while searching posts in user-profile search-post-filter, if user enters any special character in search input then it will throw the invalid regexp value error.
    
    * user post search tag filter working improved
    
    * fixed the user search filter results
    
    on entering special character at starting and then add space after special character in the string, it will allow adding space at the starting Eg. '# sayuj' --> ' sayuj'

--- src/components/posts/userProfile/PostsTabs.js
+++ src/components/posts/userProfile/PostsTabs.js
@@ -7,35 +7,105 @@ import React, {
   useEffect,
 } from "react";
 
-import { Row, Col, Menu } from "antd";
-import { DownOutlined } from "@ant-design/icons";
+import { Row, Col, Menu, Select, Input, Badge, Popover } from "antd";
+import { DownOutlined, InfoCircleOutlined } from "@ant-design/icons";
 import PostContext from "../../../context/postContext/postContext";
 
 const UserPosts = lazy(() => import("../userProfile/UserPosts"));
 const UserSavePosts = lazy(() => import("./UserSavePosts"));
 const { SubMenu, Item } = Menu;
+const { Option } = Select;
 const PostsTabs = () => {
   const [currentSelected, setCurrentSelected] = useState("user-posts");
-  const { filter, filterPosts } = useContext(PostContext);
+  const {
+    userPostfilter,
+    filterUserPosts,
+    searchUserPostsFilter,
+    clearSearchUserPostsFilter,
+    userPosts,
+    searchUserPosts,
+  } = useContext(PostContext);
 
   useEffect(() => {
     // console.log(filter);
-    filterPosts(filter);
+    filterUserPosts(userPostfilter);
+    return () => {
+      // clearing search user post filter array on leaving the page (in case if user did not clear the search field before leaving the page)
+      clearSearchUserPostsFilter();
+    };
   }, []);
   const handleClick = (e) => {
-    // console.log(e.key);
-
-    if (
-      e.key !== "item_2-menu-item_0" &&
-      e.key !== "item_2-menu-item_1" &&
-      e.key !== "filter" &&
-      e.key
-    ) {
+    if (e.key === "user-posts" || e.key === "user-saved-posts") {
       setCurrentSelected(e.key);
     }
   };
+
+  const [filterType, setfilterType] = useState("postTitle");
+
+  const handleSelect = (value) => {
+    // console.log("Selected Value", value);
+    setfilterType(value);
+  };
+
+  const selectFilter = (
+    <Select
+      defaultValue="postTitle"
+      className="select-filter"
+      size="large"
+      onChange={handleSelect}
+    >
+      <Option value="postTitle">Title</Option>
+      <Option value="postAuthor">Author</Option>
+      <Option value="tags">Tags</Option>
+      <Option value="category">Category</Option>
+    </Select>
+  );
+
+  const handleSearchFilter = (e) => {
+    {
+      e.target.value
+        ? searchUserPostsFilter(e.target.value, filterType)
+        : clearSearchUserPostsFilter();
+    }
+
+    // console.log("Search value", e.target.value, filterType);
+  };
+
   return (
     <Fragment>
+      <Row style={{ backgroundColor: "white" }}>
+        <Col
+          xs={{ span: 24 }}
+          sm={{ span: 24 }}
+          md={{ span: 20, offset: 2 }}
+          lg={{ span: 20, offset: 2 }}
+        >
+          <div
+            style={{
+              margin: "20px 20px 0px 20px",
+              fontSize: "1.2rem",
+              fontWeight: "600",
+            }}
+          >
+            Total Posts Found:
+            <Badge
+              count={
+                searchUserPosts !== null
+                  ? searchUserPosts.length
+                  : userPosts.length
+              }
+              showZero={true}
+              style={{
+                marginLeft: "10px",
+                backgroundColor: "#faad14",
+                fontSize: "1.2rem",
+                fontWeight: "600",
+                padding: "0px 10px",
+              }}
+            />
+          </div>
+        </Col>
+      </Row>
       <Row style={{ backgroundColor: "white" }}>
         {/* tabs */}
         <Col
@@ -55,18 +125,41 @@ const PostsTabs = () => {
               <Item key="user-posts"> Posts</Item>
               <Item key="user-saved-posts">Saved Posts</Item>
 
+              <Item key="filter-search">
+                {/* search post filter */}
+                <span>
+                  <Input
+                    addonAfter={selectFilter}
+                    placeholder="Search your posts by...."
+                    size="large"
+                    onChange={handleSearchFilter}
+                  />
+                </span>
+              </Item>
+              <Item key="info-icon">
+                <Popover
+                  trigger="click"
+                  title="Tags should be separated by comma ','"
+                >
+                  <InfoCircleOutlined style={{ fontSize: "32px" }} />
+                </Popover>
+              </Item>
+
               <SubMenu
                 title={
                   <span className="submenu-title-wrapper">
                     Filter
                     <DownOutlined
-                      style={{ verticalAlign: "middle", marginLeft: "10px" }}
+                      style={{
+                        verticalAlign: "middle",
+                        marginLeft: "10px",
+                      }}
                     />
                   </span>
                 }
               >
-                <Item onClick={() => filterPosts("latest")}>Latest</Item>
-                <Item onClick={() => filterPosts("oldest")}>Oldest</Item>
+                <Item onClick={() => filterUserPosts("latest")}>Latest</Item>
+                <Item onClick={() => filterUserPosts("oldest")}>Oldest</Item>
               </SubMenu>
               <Item key="filter" disabled>
                 <span
@@ -80,7 +173,7 @@ const PostsTabs = () => {
                     textTransform: "capitalize",
                   }}
                 >
-                  {filter}
+                  {userPostfilter}
                 </span>
               </Item>
             </Menu>
--- src/context/postContext/postReducer.js
+++ src/context/postContext/postReducer.js
@@ -13,7 +13,9 @@ import {
   EDIT_POST,
   CLEAR_EDIT_POST,
   UPDATE_EDIT_POST,
-  FILTER_POSTS,
+  FILTER_USER_POSTS,
+  SEARCH_USER_POSTS_FILTER,
+  CLEAR_SEARCH_USER_POSTS_FILTER,
 } from "./postTypes";
 
 const postReducer = (state, action) => {
@@ -52,6 +54,11 @@ const postReducer = (state, action) => {
     case DELETE_USER_POSTS:
       return {
         ...state,
+        searchUserPosts: state.searchUserPosts
+          ? state.searchUserPosts.filter(
+              (searchUserPost) => searchUserPost._id !== action.payload
+            )
+          : null,
         userPosts: state.userPosts.filter(
           (userPost) => userPost._id !== action.payload
         ),
@@ -120,7 +127,7 @@ const postReducer = (state, action) => {
             : userPost
         ),
       };
-    case FILTER_POSTS:
+    case FILTER_USER_POSTS:
       // console.log(state.userPosts.sort);
       // const sa = state.userPosts.sort(
       //   (a, b) => new moment(a.date) - new moment(b.date)
@@ -129,18 +136,89 @@ const postReducer = (state, action) => {
       switch (action.payload) {
         case "latest":
           state.userPosts.sort((a, b) => new Date(b.date) - new Date(a.date));
+          const searchLatestUserPosts = state.searchUserPosts
+            ? state.searchUserPosts.sort(
+                (a, b) => new Date(b.date) - new Date(a.date)
+              )
+            : null;
+
           break;
         case "oldest":
           state.userPosts.sort((a, b) => new Date(a.date) - new Date(b.date));
+          const searchOldestUserPosts = state.searchUserPosts
+            ? state.searchUserPosts.sort(
+                (a, b) => new Date(a.date) - new Date(b.date)
+              )
+            : null;
           break;
         default:
           state.userPosts.sort((a, b) => new Date(b.date) - new Date(a.date));
+          const searchDefaultUserPosts = state.searchUserPosts
+            ? state.searchUserPosts.sort(
+                (a, b) => new Date(b.date) - new Date(a.date)
+              )
+            : null;
           break;
       }
 
       return {
         ...state,
-        filter: action.payload,
+        userPostfilter: action.payload,
+      };
+    case SEARCH_USER_POSTS_FILTER:
+      let searchValue = action.payload.value.replace(/[^a-zA-Z0-9 ]/g, "");
+      searchValue = searchValue.trim();
+      // console.log("search value ", `${searchValue}`, typeof `${searchValue}`);
+      const reg = new RegExp(`${searchValue}`, "gi");
+      let searchTag = action.payload.value.split(",");
+      searchTag = searchTag.map((s) => s.replace(/[^a-zA-Z ]/g, ""));
+      searchTag = searchTag.map((s) => s.trim());
+      // console.log("Search Tag ", searchTag);
+      // let i = 0;
+      // let postFound = [];
+      // for (; i < state.userPosts.length; i++) {
+      //   if (
+      //     state.userPosts[i].tags.filter((tag) => {
+      //       let j = 0;
+      //       for (; j < searchTag.length; j++) {
+      //         if (searchTag[j] == tag) {
+      //           return true;
+      //         }
+      //       }
+      //       return false;
+      //     }).length !== 0
+      //   ) {
+      //     postFound.push(state.userPosts[i]);
+      //   }
+      // }
+      // console.log(postFound);
+      // console.log(action.payload.filter, typeof action.payload.filter);
+      return {
+        ...state,
+        searchUserPosts: state.userPosts.filter((userPost) =>
+          action.payload.filter !== "tags"
+            ? userPost[action.payload.filter].match(reg)
+            : userPost.tags.filter((tag) => {
+                let j = 0;
+
+                for (; j < searchTag.length; j++) {
+                  let reg1 = new RegExp(`${searchTag[j]}`, "gi");
+                  if (searchTag[j] !== "" && tag.match(reg1)) {
+                    return true;
+                  }
+                  // if (searchTag[j].toLowerCase() == tag.toLowerCase()) {
+                  //   return true;
+                  // }
+                }
+                return false;
+              }).length !== 0
+        ),
+      };
+
+    case CLEAR_SEARCH_USER_POSTS_FILTER:
+      return {
+        ...state,
+        searchUserPosts: null,
       };
     default:
       return state;
