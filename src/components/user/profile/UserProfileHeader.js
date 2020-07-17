3706e38311dc266ad50c4e3766bbae8c94f72145
Sayuj Sehgal <sayujsehgal@iprakash.com>
2020-07-17 13:02:55 +0530

    right align the theme switch button

--- src/components/user/profile/UserProfileHeader.js
+++ src/components/user/profile/UserProfileHeader.js
@@ -1,20 +1,20 @@
-import React, { Fragment } from "react";
-import { Row, Col, Typography } from "antd";
+import React, { Fragment } from 'react';
+import { Row, Col, Typography } from 'antd';
 // Buttons
-import CreatePostBtn from "../../buttons/user/profile/UserProfileHeader/CreatePostBtn";
-import EditProfileBtn from "../../buttons/user/profile/UserProfileHeader/EditProfileBtn";
-import GoHomeBtn from "../../buttons/global/GoHomeBtn";
-import UserTheme from "./UserTheme";
+import CreatePostBtn from '../../buttons/user/profile/UserProfileHeader/CreatePostBtn';
+import EditProfileBtn from '../../buttons/user/profile/UserProfileHeader/EditProfileBtn';
+import GoHomeBtn from '../../buttons/global/GoHomeBtn';
+import UserTheme from './UserTheme';
 const { Title } = Typography;
 const UserProfileHeader = () => {
   return (
     <Fragment>
       {/* buttons */}
-      <Row className="backgroundContent" style={{ paddingTop: "20px" }}>
+      <Row className='backgroundContent' style={{ paddingTop: '20px' }}>
         {/* go back btn */}
 
         <Col xs={{ span: 20, offset: 1 }}>
-          <GoHomeBtn margin="0px 0px 20px 0px" shape="round" />
+          <GoHomeBtn margin='0px 0px 20px 0px' shape='round' />
         </Col>
 
         <Col
@@ -23,7 +23,7 @@ const UserProfileHeader = () => {
           md={{ span: 22, offset: 2 }}
           lg={{ span: 22 }}
         >
-          <Title level={4} style={{ marginLeft: "18px" }}>
+          <Title level={4} style={{ marginRight: '18px', textAlign: 'right' }}>
             Select Theme: <UserTheme />
           </Title>
         </Col>
