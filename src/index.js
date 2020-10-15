fb94e91939922fe190a2162a786f2f5c6558cc8b
Sayuj <sayujsehgal@iprakash.com>
2020-10-15 14:11:23 +0530

    updated for deployment

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React, { lazy, Suspense } from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import React, { lazy, Suspense } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 // import App from "./App";
-const App = lazy(() => import("./App"));
+const App = lazy(() => import('./App'));
 
 ReactDOM.render(
-  <Suspense fallback={<div style={{ fontSize: "50px" }}>Loading App1....</div>}>
+  <Suspense fallback={<div style={{ fontSize: '50px' }}>Loading App1....</div>}>
     <App />
   </Suspense>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
